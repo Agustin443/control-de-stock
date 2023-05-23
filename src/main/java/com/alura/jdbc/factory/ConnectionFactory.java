3d025b3beab7cad9c9d1290991db179cb45960f6
Agustin443 <Agustin.cordero001@gmail.com>
2023-05-22 20:11:43 -0400

    Aniadiendo pool de conexiones en la aplicacion

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,16 +1,28 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+		var pooledDataSouce = new ComboPooledDataSource();
+		pooledDataSouce.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSouce.setUser("root");
+		pooledDataSouce.setPassword("Nomeacuerdo1");
+		
+		this.datasource = pooledDataSouce;
+	}
+	
 	public Connection recuperaConexion() throws SQLException{
 		
-		return DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-                "root",
-                "Nomeacuerdo1");
+		return this.datasource.getConnection();
+                
 	}
 }
