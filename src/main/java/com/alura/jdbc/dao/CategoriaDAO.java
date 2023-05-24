6066b9d594bf1d4e75c111fa366863b1c47c0679
Agustin443 <Agustin.cordero001@gmail.com>
2023-05-24 01:51:16 -0400

    Refinando el reporte con la base de datos implementando inner join

--- src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
+++ src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alura.jdbc.modelo.Categoria;
+import com.alura.jdbc.modelo.Producto;
 
 public class CategoriaDAO {
 	
@@ -40,6 +41,52 @@ public class CategoriaDAO {
 						var categoria = new Categoria(resultSet.getInt("ID"), resultSet.getString("NOMBRE"));
 						
 						resultado.add(categoria);
+					}
+				};
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return resultado;
+	}
+
+	public List<Categoria> listarConProductos() {
+		List<Categoria> resultado =  new ArrayList<>();
+		
+		try {
+			
+			var querySelect = "SELECT C.ID, C.NOMBRE, P.ID, P.NOMBRE, P.CANTIDAD FROM CATEGORIA C " 
+					+ "INNER JOIN PRODUCTO P ON C.ID = P.CATEGORIA_ID";
+			
+			System.out.println(querySelect);
+			
+			final PreparedStatement statement =  con.prepareStatement(querySelect);
+			
+			try(statement){
+				
+				final ResultSet resultSet =  statement.executeQuery();
+				
+				try(resultSet){
+				
+					while(resultSet.next()) {
+					
+						Integer categoriaId = resultSet.getInt("ID");
+						String categoriaNombre = resultSet.getString("NOMBRE");
+					
+						var categoria = resultado
+								.stream()
+								.filter(cat -> cat.getId().equals(categoriaId))
+								.findAny().orElseGet(()->
+						{Categoria cat = new Categoria(categoriaId, categoriaNombre);
+						
+						resultado.add(cat);
+						return cat;
+						});
+						
+						Producto producto = new Producto(resultSet.getInt("P.ID"), resultSet.getString("P.NOMBRE"), resultSet.getInt("P.CANTIDAD"));
+							
+						categoria.agregar(producto);
 					}
 				};
 			}
--- src/main/java/com/alura/jdbc/view/ReporteFrame.java
+++ src/main/java/com/alura/jdbc/view/ReporteFrame.java
@@ -18,13 +18,11 @@ public class ReporteFrame extends JFrame {
 
     private CategoriaController categoriaController;
     
-    private ProductoController productoController;
 
     public ReporteFrame(ControlDeStockFrame controlDeStockFrame) {
         super("Reporte de produtos del stock");
 
         this.categoriaController = new CategoriaController();
-        this.productoController = new ProductoController();
 
         Container container = getContentPane();
         setLayout(null);
@@ -53,7 +51,7 @@ public class ReporteFrame extends JFrame {
         	
         	modelo.addRow(new Object[] {categoria});
         
-	        var productos = this.productoController.listar(categoria);
+	        var productos = categoria.getProductos();
 	        
 	        productos.forEach(producto -> modelo.addRow(new Object[] {
 	        		"",
